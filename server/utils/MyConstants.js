93225368446b7a7573da2698e6e6225177efa0b5
hung <hungle0921@gmail.com>
2024-07-21 00:20:34 +0700

    Initial commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,13 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.gujssut.mongodb.net',
+    DB_USER: 'db_hungle0921',
+    DB_PASS: 'db_hung2192003',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'hungle0921@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'sdufqtmfvszyjhau',
+    JWT_SECRET: 'KHOA_BI_MAT',
+    JWT_EXPIRES: '98392138719238172837918', // in milliseconds
+  };
+  
+  module.exports = MyConstants;
+  
